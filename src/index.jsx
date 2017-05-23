eeb300d328893119a8b48ce6c0f961fa6a62f11e
Irene <irene.nikkarinen@helsinki.fi>
2017-05-23 11:37:20 +0300

    Implement embedding

--- src/index.jsx
+++ src/index.jsx
@@ -8,13 +8,14 @@ import createStore from 'state/store';
 import './styles';
 import App from './components/app';
 
-const store = createStore();
-
-const rootElement = document.getElementById('app');
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement,
-);
+window.initTyponator = function () {
+  document.querySelectorAll('.typonator-widget').forEach((element) => {
+    const store = createStore();
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      element,
+    );
+  });
+};
